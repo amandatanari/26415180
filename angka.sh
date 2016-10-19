c5c87dbb28cf18d42f9f9798f2f62b6d5b7f3f94
amandatanari <m26415180@john.petra.ac.id>
2016-10-19 15:55:18 +0700

    very simple calc

---
+++ angka.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Masukkan angka : "
+read n
+
+echo "Hasil tambah : "$n+$n
+echo "Hasil kali : "$n*$n
+echo "Hasil pangkat 2 : "$n*$n
+echo "Hasil pangkat 3 : "$n*$n*$n
+echo "Hasil pangkat 4 : "$n*$n*$n*$n
