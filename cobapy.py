b6ea8e512966c562eedd843ad272be118d85c2fb
amandatanari <m26415180@john.petra.ac.id>
2016-10-26 18:04:36 +0700

    first weekly push

---
+++ cobapy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+print "Hello, Python!"
+
+if True:
+	print "True"
+else:
+	print "False"
+
+satu=1
+dua = 2
+tiga = 3
+
+total = satu + dua + tiga
+print total
+
+days = ['One', 'Two', 'Three', 'Four']
+print days
+
+word = 'kata'
+print word
+
+sentence = "sentence: This is how we do it."
+print sentence
+paragraph = """Paragraph: How to make a paragraph is
+that you should use quotation mark three times."""
+print paragraph
+name = "name: TOS" #lalala ini comment
+print name
+raw_input("first line \nsecondline \nPress the enter key to exit.")
+
+import sys;
+x = 'foo'; sys.stdout.write(x+'\n')
+expression = true #salah disini
+if expression :
+	suite
+elif expression :
+	suite
+else :
+	suite
