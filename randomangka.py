30049416b0004057b4331e7a1886934ab965630f
amandatanari <m26415180@john.petra.ac.id>
2016-11-02 20:38:08 +0700

    yeah

--- randomangka.py
+++ randomangka.py
@@ -5,11 +5,11 @@ angka = random.randrange(1,101,1)
 win = false
 print 'cheat: '+str(angka)
 for i in range(5):
-be = raw_input('Masukkan tebakan ke-'+str(i+1)+':')
+input = raw_input('Masukkan tebakan ke-'+str(i+1)+':')
 
-if int (be) < angka:
+if int (input) < angka:
 print 'too small.'
-elif int (be) > angka:
+elif int (input) > angka:
 print 'TOO BIG!'
 else
 win = True
